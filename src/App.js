b70274698c7d3d87a4aa5000c76aed894ffc43e0
obleus <odney@Odneys-MacBook-Air.local>
2023-01-18 10:14:50 -0600

    finished file and folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material'
+
 import './App.css'
+import ExerciseDetail from './Pages/ExerciseDetail';
+import Home from './Pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+
+
 const App = () => {
   return (
-    <div>App</div>
+    <Box width='400px'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   )
 }
 
